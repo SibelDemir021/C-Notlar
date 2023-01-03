b21bbd8b9d6d0576f839d77fda6a9e61ad724259
SibelDemir021 <121857587+SibelDemir021@users.noreply.github.com>
2023-01-03 12:40:14 +0300

    03,011565

---
+++ iki sayının toplamı2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi1, sayi2 , tpl;
+            Console.WriteLine("lutfen sayi giriniz");
+            sayi1 = Convert.ToInt32(Console.ReadLine());
+            sayi2 = Convert.ToInt32(Console.ReadLine());
+            tpl = sayi1 + sayi2;
+            Console.WriteLine("tpl=" + tpl);
+            Console.ReadKey();
+
+        }
+    }
+}
